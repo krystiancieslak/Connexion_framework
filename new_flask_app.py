81d83f29bc0255488bc0a4c73eca684c91b58e47
Krystian <krystian.cieslak.kontakt@gmail.com>
2022-11-11 17:24:33 +0100

    deleted hardcoded port description

--- new_flask_app.py
+++ new_flask_app.py
@@ -10,4 +10,4 @@ def home():
 if __name__ == "__main__":
     app.add_api('my_api.yaml')
     port = int(os.environ.get('PORT', 5001))
-    app.run(port=5001, host='0.0.0.0')
+    app.run(port=port, host='0.0.0.0')
