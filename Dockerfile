d0fa45fa534a7c5094ed67dc2c30d8dd952430e3
Krystian <krystian.cieslak.kontakt@gmail.com>
2022-11-11 17:09:54 +0100

    Working home page

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ WORKDIR /.
 COPY requirements.txt requirements.txt
 RUN pip3 install -r requirements.txt
 COPY . .
-CMD [ "flask", "--app" , "new_flask_app.py", "run", "--host=0.0.0.0"]
+CMD [ "python3", "new_flask_app.py"]
--- new_flask_app.py
+++ new_flask_app.py
@@ -1,16 +1,13 @@
-from flask import Flask
 import connexion
-
+import os
 
 app = connexion.App(__name__)
-app.add_api('my_api.yaml')
-app.run(port=5001)
 
 @app.route("/")
 def home():
     return "hello there"
 
 if __name__ == "__main__":
+    app.add_api('my_api.yaml')
     port = int(os.environ.get('PORT', 5001))
-    app.add_api("my_api.yaml")
-    app.run(host='0.0.0.0')
+    app.run(port=5001, host='0.0.0.0')
